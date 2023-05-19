7d9d17c85bdde10129ed787cc678c16a0175821d
Rhys Bushnell <rhys.bushnell@ensono.com>
2023-05-19 13:15:29 +0100

    Update Permissions

--- deploy/azure/infra/main.tf
+++ deploy/azure/infra/main.tf
@@ -260,6 +260,6 @@ resource "azurerm_role_assignment" "storage_role_config_private" {
 
 resource "azurerm_role_assignment" "storage_role_context" {
   scope                = module.adls_private.storage_account_ids[0]
-  role_definition_name = var.adls_datalake_role_adf
+  role_definition_name = "Storage Account Contributor"
   principal_id         = data.azurerm_client_config.current.object_id
 }
