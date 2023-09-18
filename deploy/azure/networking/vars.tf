66d839938937668fdb4bf568a2bd2871f460acbb
Satenderrathee <satender.rathee07@gmail.com>
2023-09-18 14:52:23 +0100

    updated

--- deploy/azure/networking/vars.tf
+++ deploy/azure/networking/vars.tf
@@ -194,6 +194,30 @@ variable "network_details" {
         }
     } },
 
+    "amido-stacks-euw-de-qa-prod" = {
+      name                = "amido-stacks-euw-de-qa-prod"
+      address_space       = ["10.6.0.0/16"]
+      dns_servers         = []
+      resource_group_name = "amido-stacks-euw-de-qa-prod-network"
+      is_hub              = false
+      link_to_private_dns = true
+      subnet_details = {
+        "amido-stacks-euw-de-qa-prod-pe" = {
+          sub_name                                      = "amido-stacks-euw-de-qa-prod-pe"
+          sub_address_prefix                            = ["10.6.1.0/24"]
+          private_endpoint_network_policies_enabled     = true
+          private_link_service_network_policies_enabled = true
+          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
+        },
+        "amido-stacks-euw-de-qa-prod" = {
+          sub_name                                      = "amido-stacks-euw-de-qa-prod"
+          sub_address_prefix                            = ["10.6.2.0/24"]
+          private_endpoint_network_policies_enabled     = true
+          private_link_service_network_policies_enabled = true
+          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
+        }
+    } },
+
   }
 }
 
