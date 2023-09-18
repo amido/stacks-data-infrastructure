b6eae6ba6a8668313e92323a4e2f1384fea0735b
adurkan-amido <andy.durkan@amido.com>
2023-09-18 15:41:23 +0100

    update ingest test

--- de_workloads/ingest/ingest_azure_sql_andytest/data_factory/adf_pipelines.tf
+++ de_workloads/ingest/ingest_azure_sql_andytest/data_factory/adf_pipelines.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group_template_deployment" "pipeline_Ingest_azure_sql_andytest" {
-  name                = "pipeline_Ingest_azure_sql_andytest"
+resource "azurerm_resource_group_template_deployment" "pipeline_ingest_azure_sql_andytest" {
+  name                = "pipeline_ingest_azure_sql_andytest"
   resource_group_name = var.data_factory_resource_group_name
   deployment_mode     = var.arm_deployment_mode
   parameters_content = jsonencode({
--- de_workloads/ingest/ingest_azure_sql_andytest/data_factory/adf_triggers.tf
+++ de_workloads/ingest/ingest_azure_sql_andytest/data_factory/adf_triggers.tf
@@ -1,5 +1,5 @@
-resource "azurerm_data_factory_trigger_tumbling_window" "trigger_Ingest_azure_sql_andytest" {
-  name            = "trigger_Ingest_azure_sql_andytest"
+resource "azurerm_data_factory_trigger_tumbling_window" "trigger_ingest_azure_sql_andytest" {
+  name            = "trigger_ingest_azure_sql_andytest"
   data_factory_id = data.azurerm_data_factory.factory.id
   start_time      = "2010-01-01T00:00:00Z"
   end_time        = "2011-12-31T23:59:59Z"
@@ -10,7 +10,7 @@ resource "azurerm_data_factory_trigger_tumbling_window" "trigger_Ingest_azure_sq
   activated       = false
 
   pipeline {
-    name = "Ingest_azure_sql_andytest"
+    name = "ingest_azure_sql_andytest"
     parameters = {
       window_start = "@{formatDateTime(trigger().outputs.windowStartTime,'yyyy-MM-dd')}",
       window_end   = "@{formatDateTime(trigger().outputs.windowEndTime,'yyyy-MM-dd')}"
@@ -18,6 +18,6 @@ resource "azurerm_data_factory_trigger_tumbling_window" "trigger_Ingest_azure_sq
   }
 
   depends_on = [
-    azurerm_resource_group_template_deployment.pipeline_Ingest_azure_sql_andytest
+    azurerm_resource_group_template_deployment.pipeline_ingest_azure_sql_andytest
   ]
 }
