11353b02440372c2e111c9e317fa8ca2fd2be81d
Satenderrathee <satender.rathee07@gmail.com>
2023-09-28 11:36:47 +0100

    updated

--- deploy/azure/infra/locals.tf
+++ deploy/azure/infra/locals.tf
@@ -32,13 +32,13 @@ locals {
   #   endswith(connection.properties.privateLinkServiceConnectionState.description, azurerm_data_factory_managed_private_endpoint.sql_pe.name)
   # ])
 
-  # ## ADB
-  # adb_private_endpoint_connection_name = one([
-  #   for connection in jsondecode(data.azapi_resource.adb_private_endpoint_connection.output).properties.privateEndpointConnections
-  #   : connection.name
-  #   if
-  #   endswith(connection.properties.privateLinkServiceConnectionState.description, azurerm_data_factory_managed_private_endpoint.db_pe.name)
-  # ])
+  ## ADB
+  adb_private_endpoint_connection_name = one([
+    for connection in jsondecode(data.azapi_resource.adb_private_endpoint_connection.output).properties.privateEndpointConnections
+    : connection.name
+    if
+    endswith(connection.properties.privateLinkServiceConnectionState.description, azurerm_data_factory_managed_private_endpoint.db_pe.name)
+  ])
 
   # ## ADB auth
   adb_auth_private_endpoint_connection_name = one([
