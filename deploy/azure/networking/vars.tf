3439bd14c97a7c4bff35326c75405b9a2650b928
Zaid Khider <zaid.khider@kingfisher.com>
2023-12-22 12:22:16 +0000

    Add Russell changes

--- deploy/azure/networking/vars.tf
+++ deploy/azure/networking/vars.tf
@@ -41,24 +41,6 @@ variable "resource_group_location" {
   default = "uksouth"
 }
 
-
-# Each region must have corresponding a shortend name for resource naming purposes
-variable "location_name_map" {
-  type = map(string)
-
-  default = {
-    northeurope   = "eun"
-    westeurope    = "euw"
-    uksouth       = "uks"
-    ukwest        = "ukw"
-    eastus        = "use"
-    eastus2       = "use2"
-    westus        = "usw"
-    eastasia      = "ase"
-    southeastasia = "asse"
-  }
-}
-
 ############################################
 # NETWORK INFORMATION
 ############################################
@@ -76,174 +58,6 @@ variable "link_dns_network" {
   default     = false
 }
 
-variable "network_details" {
-  type = map(object({
-    name                = string
-    address_space       = list(string)
-    dns_servers         = list(string)
-    resource_group_name = string
-    is_hub              = bool
-    link_to_private_dns = bool
-    subnet_details = map(object({
-      sub_name                                      = string
-      sub_address_prefix                            = list(string)
-      private_endpoint_network_policies_enabled     = bool
-      private_link_service_network_policies_enabled = bool
-      service_endpoints                             = list(string)
-      })
-    )
-
-  }))
-
-  default = {
-    "amido-stacks-euw-de-hub" = {
-      name                = "amido-stacks-euw-de-hub"
-      address_space       = ["10.2.0.0/16"]
-      dns_servers         = []
-      resource_group_name = "amido-stacks-euw-de-hub-network"
-      is_hub              = true
-      link_to_private_dns = true
-      subnet_details = {
-        "amido-stacks-euw-de-hub" = {
-          sub_name                                      = "amido-stacks-euw-de-hub"
-          sub_address_prefix                            = ["10.2.1.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        },
-        "build-agent" = {
-          sub_name                                      = "build-agent"
-          sub_address_prefix                            = ["10.2.2.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        }
-
-    } },
-
-
-    "amido-stacks-euw-de-nonprod" = {
-      name                = "amido-stacks-euw-de-nonprod"
-      address_space       = ["10.3.0.0/16"]
-      dns_servers         = []
-      resource_group_name = "amido-stacks-euw-de-nonprod-network"
-      is_hub              = false
-      link_to_private_dns = true
-      subnet_details = {
-        "amido-stacks-euw-de-nonprod-pe" = {
-          sub_name                                      = "amido-stacks-euw-de-nonprod-pe"
-          sub_address_prefix                            = ["10.3.1.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        },
-        "amido-stacks-euw-de-nonprod" = {
-          sub_name                                      = "amido-stacks-euw-de-nonprod"
-          sub_address_prefix                            = ["10.3.2.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        }
-    } },
-
-    "amido-stacks-euw-de-prod" = {
-      name                = "amido-stacks-euw-de-prod"
-      address_space       = ["10.4.0.0/16"]
-      dns_servers         = []
-      resource_group_name = "amido-stacks-euw-de-prod-network"
-      is_hub              = false
-      link_to_private_dns = true
-      subnet_details = {
-        "amido-stacks-euw-de-prod-pe" = {
-          sub_name                                      = "amido-stacks-euw-de-prod-pe"
-          sub_address_prefix                            = ["10.4.1.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        },
-        "amido-stacks-euw-de-prod" = {
-          sub_name                                      = "amido-stacks-euw-de-prod"
-          sub_address_prefix                            = ["10.4.2.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        }
-    } },
-
-    "amido-stacks-euw-de-qa-nonprod" = {
-      name                = "amido-stacks-euw-de-qa-nonprod"
-      address_space       = ["10.5.0.0/16"]
-      dns_servers         = []
-      resource_group_name = "amido-stacks-euw-de-qa-nonprod-network"
-      is_hub              = false
-      link_to_private_dns = true
-      subnet_details = {
-        "amido-stacks-euw-de-qa-nonprod-pe" = {
-          sub_name                                      = "amido-stacks-euw-de-qa-nonprod-pe"
-          sub_address_prefix                            = ["10.5.1.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        },
-        "amido-stacks-euw-de-qa-nonprod" = {
-          sub_name                                      = "amido-stacks-euw-de-qa-nonprod"
-          sub_address_prefix                            = ["10.5.2.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        }
-    } },
-    "amido-stacks-euw-de-qa-prod" = {
-      name                = "amido-stacks-euw-de-qa-prod"
-      address_space       = ["10.6.0.0/16"]
-      dns_servers         = []
-      resource_group_name = "amido-stacks-euw-de-qa-prod-network"
-      is_hub              = false
-      link_to_private_dns = true
-      subnet_details = {
-        "amido-stacks-euw-de-qa-prod-pe" = {
-          sub_name                                      = "amido-stacks-euw-de-qa-prod-pe"
-          sub_address_prefix                            = ["10.6.1.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        },
-        "amido-stacks-euw-de-qa-prod" = {
-          sub_name                                      = "amido-stacks-euw-de-qa-prod"
-          sub_address_prefix                            = ["10.6.2.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        }
-    } },
-
-    "amido-stacks-euw-de-test-nonprod" = {
-      name                = "amido-stacks-euw-de-test-nonprod"
-      address_space       = ["10.7.0.0/16"]
-      dns_servers         = []
-      resource_group_name = "amido-stacks-euw-de-test-nonprod-network"
-      is_hub              = false
-      link_to_private_dns = true
-      subnet_details = {
-        "amido-stacks-euw-de-test-nonprod-pe" = {
-          sub_name                                      = "amido-stacks-euw-de-test-nonprod-pe"
-          sub_address_prefix                            = ["10.7.1.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        },
-        "amido-stacks-euw-de-test-nonprod" = {
-          sub_name                                      = "amido-stacks-euw-de-test-nonprod"
-          sub_address_prefix                            = ["10.7.2.0/24"]
-          private_endpoint_network_policies_enabled     = true
-          private_link_service_network_policies_enabled = true
-          service_endpoints                             = ["Microsoft.AzureActiveDirectory", "Microsoft.KeyVault", "Microsoft.ServiceBus", "Microsoft.Sql", "Microsoft.Storage"]
-        }
-    } },
-
-  }
-}
-
 variable "create_hub_fw" {
   default     = false
   type        = bool
@@ -254,12 +68,6 @@ variable "create_fw_public_ip" {
   default     = false
   type        = bool
   description = "Determines if the networking modules creates a firewall IP address or not."
-}
-
-variable "create_private_dns_zone" {
-  default     = true
-  type        = bool
-  description = "Determines if the networking modules creates a private dns zone."
 }
 
 variable "dns_zone_name" {
@@ -294,10 +102,4 @@ variable "vmss_subnet_name" {
   default     = "build-agent"
   type        = string
   description = "The subnet name which the VMSS will be provisioned."
-}
-
-variable "build_agent_ip" {
-  default     = ""
-  description = "IP address of the build agent"
-  type        = string
 }
